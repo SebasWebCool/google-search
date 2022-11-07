fbbfbc88a9603ba573fb5c96b088ff52a10f505b
Sebas <sebasweb2000@gmail.com>
2022-11-07 15:43:49 -0400

    better at video

--- src/results/Videos.jsx
+++ src/results/Videos.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { setLoadingTrue } from '../store/slice/isLoading.slice'
+import { setTerm } from '../store/slice/term.slice'
 
 const Videos = ({ results, setCursorVideos }) => {
 
-    const [previousPage, setPreviousPage] = useState()
+    // const [previousPage, setPreviousPage] = useState()
 
+
+    const dispatch = useDispatch()
 
     const handleNext = () => {
         // setPreviousPage(results.cursorNext)
@@ -53,16 +57,16 @@ const Videos = ({ results, setCursorVideos }) => {
                         res.type == "video" ?
 
                             <div key={i} className='sm:w-2/6 w-4/6'>
-                                <a className='w-full ' href={`https://www.youtube.com/watch?v=${res.video.videoId}`}>
+                                <a target={'_blank'} className='w-full ' href={`https://www.youtube.com/watch?v=${res.video.videoId}`}>
                                     <img className='w-full transition ease-in-out delay-100 duration-300  hover:scale-125' src={res.video.thumbnails && res.video.thumbnails[0].url} alt="" />
                                 </a>
 
-                                <a href={`https://www.youtube.com/watch?v=${res.video.videoId}`}>
+                                <a target={'_blank'} href={`https://www.youtube.com/watch?v=${res.video.videoId}`}>
                                     <h2 className='text-lg transition ease-in-out delay-150 duration-300 hover:text-blue-500 '>
                                         {res.video.title.length > 30 ? res.video.title.slice(0, 40) + '...' : res.video.title}
                                     </h2>
                                 </a>
-                                <a className='transition ease-in-out delay-150 duration-300 hover:text-blue-500' href={`https://www.youtube.com/watch?v=${res.video.author.channelId}`}>
+                                <a target={'_blank'} className='transition ease-in-out delay-150 duration-300 hover:text-blue-500' href={`https://www.youtube.com/channel/${res.video.author.channelId}`}>
                                     <p>
                                         {res.video.author.title}
                                     </p>
@@ -80,7 +84,7 @@ const Videos = ({ results, setCursorVideos }) => {
                 {/* {
                     previousPage ? <button onClick={() => handlePrevious()}>Previous</button> : ""
                 } */}
-                <button onClick={() => handleNext()} >Next</button>
+                <button className='px-4 py-2 transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300' onClick={() => handleNext()} >Next</button>
             </div>
 
         </section>
